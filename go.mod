d2901ca4ff1bd33228226b1fb5470cc1b505c470
Mohammed Ibrahim <HNU.24.308.Muhammad-Abdulnabi@hnu.edu.eg>
2025-04-02 10:33:38 +0200

    Add: Logging and Rate Limiting

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module restApi/web-Service
 
 go 1.24.1
 
-require github.com/gofiber/fiber/v2 v2.52.6
+require (
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.10.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
@@ -12,9 +16,10 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"net/http"
-	"github.com/gofiber/fiber/v2"
 	"strconv"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/sirupsen/logrus"
 )
 
 type Films struct {
-	Id	int
-	Title string
-	Year  int
+	Id       int
+	Title    string
+	Year     int
 	FilmType string
 }
 
@@ -21,30 +25,41 @@ var films = []Films{
 	{Id: 5, Title: "The Lord of the Rings: The Return of the King", Year: 2003, FilmType: "Fantasy"},
 }
 
+var logger = logrus.New()
+
 func getFilms(c *fiber.Ctx) error {
+	logger.Info("Fetching all films")
 	return c.JSON(films)
 }
 
 func getFilmByID(c *fiber.Ctx) error {
 	id := c.Params("id")
-intID, err := strconv.Atoi(id)
-if err != nil {
-	return c.Status(http.StatusBadRequest).SendString("Invalid ID")
-}
-for _, film := range films {
-	if film.Id == intID {
-		return c.JSON(film)
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"id":    id,
+		}).Error("Invalid ID")
+		return c.Status(http.StatusBadRequest).SendString("Invalid ID")
 	}
-}
-return c.Status(http.StatusNotFound).SendString("Film not found")
+	for _, film := range films {
+		if film.Id == intID {
+			logger.WithField("id", intID).Info("Film found")
+			return c.JSON(film)
+		}
+	}
+	logger.WithField("id", intID).Warn("Film not found")
+	return c.Status(http.StatusNotFound).SendString("Film not found")
 }
 
 func addFilm(c *fiber.Ctx) error {
 	film := new(Films)
 	if err := c.BodyParser(film); err != nil {
+		logger.WithField("error", err.Error()).Error("Invalid film data")
 		return c.Status(http.StatusBadRequest).SendString("Invalid film")
 	}
 	films = append(films, *film)
+	logger.WithField("film", film).Info("Film added")
 	return c.JSON(film)
 }
 
@@ -52,18 +67,25 @@ func updateFilm(c *fiber.Ctx) error {
 	id := c.Params("id")
 	intID, err := strconv.Atoi(id)
 	if err != nil {
+		logger.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"id":    id,
+		}).Error("Invalid ID")
 		return c.Status(http.StatusBadRequest).SendString("Invalid ID")
 	}
 	film := new(Films)
 	if err := c.BodyParser(film); err != nil {
+		logger.WithField("error", err.Error()).Error("Invalid film data")
 		return c.Status(http.StatusBadRequest).SendString("Invalid film")
 	}
 	for i, f := range films {
 		if f.Id == intID {
 			films[i] = *film
+			logger.WithField("film", film).Info("Film updated")
 			return c.JSON(film)
 		}
 	}
+	logger.WithField("id", intID).Warn("Film not found for update")
 	return c.Status(http.StatusNotFound).SendString("Film not found")
 }
 
@@ -72,27 +94,49 @@ func deleteFilm(c *fiber.Ctx) error {
 	for i, film := range films {
 		intID, err := strconv.Atoi(id)
 		if err != nil {
+			logger.WithFields(logrus.Fields{
+				"error": err.Error(),
+				"id":    id,
+			}).Error("Invalid ID")
 			return c.Status(http.StatusBadRequest).SendString("Invalid ID")
 		}
 		if film.Id == intID {
 			films = append(films[:i], films[i+1:]...)
+			logger.WithField("id", intID).Info("Film deleted")
 			return c.SendString("Film is deleted")
 		}
 	}
+	logger.WithField("id", id).Warn("Film not found for deletion")
 	return c.Status(http.StatusNotFound).SendString("Film not found")
 }
 
-
 func main() {
-    app := fiber.New()
+	app := fiber.New()
+
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetLevel(logrus.InfoLevel)
+	/*
+	using Sliding Window Rate Limiting instead of Fixed Window Rate Limiting
+	-> becauese it allows for a more flexible and responsive rate limiting strategy.
+	-> It can help to smooth out spikes in traffic and provide a more consistent user experience.
+	
+	---> The rate limit is set to 4 requests per 20 seconds.
+	*/
+	app.Use(limiter.New(limiter.Config{
+		Max:        4,
+		Expiration: 20 * time.Second,
+		LimitReached: func(c *fiber.Ctx) error {
+			logger.Warn("Rate limit exceeded")
+			return c.Status(http.StatusTooManyRequests).SendString("Rate limit exceeded")
+		},
+	}))
 
 	app.Get("/films", getFilms)
-
 	app.Get("/films/:id", getFilmByID)
 	app.Post("/films", addFilm)
-
 	app.Put("/films/:id", updateFilm)
 	app.Delete("/films/:id", deleteFilm)
 
-    app.Listen(":3000")
+	logger.Info("Starting server on :3000")
+	app.Listen(":3000")
 }
